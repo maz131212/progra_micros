5b3bdbb075676b7ec34ddec095ae5b5818316844
maz131212 <68131269+maz131212@users.noreply.github.com>
2021-02-10 15:18:14 -0600

    cambio de carpeta lab1

--- lab1_micros/Lab1.X/nbproject/Makefile-local-default.mk
+++ lab1_micros/Lab1.X/nbproject/Makefile-local-default.mk
@@ -14,25 +14,24 @@
 # You can invoke make with the values of the macros:
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
-SHELL=cmd.exe
-PATH_TO_IDE_BIN=C:/Program Files/Microchip/MPLABX/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/
+PATH_TO_IDE_BIN=/Applications/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=C:/Program Files/Microchip/MPLABX/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
+PATH:=/Applications/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="C:\Program Files\Microchip\MPLABX\v5.45\sys\java\zulu8.40.0.25-ca-fx-jre8.0.222-win_x64/bin/"
+MP_JAVA_PATH="/Applications/microchip/mplabx/v5.45/sys/java/zulu8.48.0.53-ca-fx-jre8.0.265-macosx_x64/zulu-8.jre/Contents/Home/bin/"
 OS_CURRENT="$(shell uname -s)"
 # MP_CC is not defined
 # MP_CPPC is not defined
 # MP_BC is not defined
-MP_AS="C:\Program Files\Microchip\xc8\v2.31\pic-as\bin\pic-as.exe"
-MP_LD="C:\Program Files\Microchip\xc8\v2.31\pic-as\bin\pic-as.exe"
+MP_AS="/Applications/microchip/xc8/v2.31/pic-as/bin/pic-as"
+MP_LD="/Applications/microchip/xc8/v2.31/pic-as/bin/pic-as"
 # MP_AR is not defined
-DEP_GEN=${MP_JAVA_PATH}java -jar "C:/Program Files/Microchip/MPLABX/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+DEP_GEN=${MP_JAVA_PATH}java -jar "/Applications/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
 # MP_CC_DIR is not defined
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR="C:\Program Files\Microchip\xc8\v2.31\pic-as\bin"
-MP_LD_DIR="C:\Program Files\Microchip\xc8\v2.31\pic-as\bin"
+MP_AS_DIR="/Applications/microchip/xc8/v2.31/pic-as/bin"
+MP_LD_DIR="/Applications/microchip/xc8/v2.31/pic-as/bin"
 # MP_AR_DIR is not defined
 # MP_BC_DIR is not defined
-DFP_DIR=C:/Program Files/Microchip/MPLABX/v5.45/packs/Microchip/PIC16Fxxx_DFP/1.2.33
+DFP_DIR=/Applications/microchip/mplabx/v5.45/packs/Microchip/PIC16Fxxx_DFP/1.2.33
